3a6092ed5a4278684a78635ad4726e51a6b65538
ChooYongHan <83595230+ChooYongHan@users.noreply.github.com>
2021-08-22 14:19:33 +0800

    Day 1 Assignment submitted

---
+++ Assignment 1.py
@@ -0,0 +1,9 @@
+#Assignment 1
+#Name: CHOO YONG HAN
+
+num1=int(input("Enter number x: "))
+num2=int(input("Enter number y: "))
+
+result=num1**num2
+
+print("Result of" ,num1, "to power" , num2, "is" ,result)
